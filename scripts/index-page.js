0bc9add0d77eef33bc7c65f42bf284aa877f7d71
Kevin Ip <ipkevin@gmail.com>
2023-02-17 16:23:53 -0800

    add sort function comments array before displaying to site; replace dark black color variable name to refer to it by purpose

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,7 +1,12 @@
 
 // Comments Array
 let commentsDB = [
-{
+    {
+        name: "Miles Acosta",
+        date: "12/20/2020",
+        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+    },
+    {
     name: "Connor Walton",
     date: "02/17/2021",
     comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
@@ -11,11 +16,7 @@ let commentsDB = [
     date: "01/09/2021",
     comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
 },
-{
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-}
+
 ];
 
 const formElem = document.getElementById('comments-form');
@@ -83,7 +84,11 @@ function updateComments(){
         // clear the comments__items already on the page outside of the form
         clearList();
 
-        // Now iterate thru all of the comments from the DB and display them to the page
+        // Ensure the comments array is sorted with newest first
+        commentsDB.sort( (a,b) => {
+            return new Date(b.date) - new Date(a.date);
+        })
+        // Now iterate thru the comments array and display them to the page
         commentsDB.forEach(displayComment);
 
     } else {
